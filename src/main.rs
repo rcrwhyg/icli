aff3861b0351a3cfde0925f4316c611e249a4832
vilen <rcrwhyg@sina.com>
2024-08-26 15:55:56 +0800

    feature: add password generator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use rcli::{process_csv, Opts, Subcommand};
+use rcli::{process_csv, process_genpass, Opts, Subcommand};
 
 fn main() -> anyhow::Result<()> {
     let opts: Opts = Opts::parse();
@@ -12,6 +12,13 @@ fn main() -> anyhow::Result<()> {
             };
             process_csv(&opts.input, output, opts.format)?;
         }
+        Subcommand::GenPass(opts) => process_genpass(
+            opts.length,
+            opts.uppercase,
+            opts.lowercase,
+            opts.number,
+            opts.symbol,
+        )?,
     }
 
     Ok(())
