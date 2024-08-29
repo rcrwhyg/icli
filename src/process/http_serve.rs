f7b840772723f967e6075213e215dfaff9f489e4
rcrwhyg <56500577+rcrwhyg@users.noreply.github.com>
2024-08-30 00:06:05 +0800

    feature: add tower-http support

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -5,6 +5,7 @@ use axum::{
     Router,
 };
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
 
 #[derive(Debug)]
@@ -16,11 +17,12 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> anyhow::Result<()> 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!("Serving {:?} on port {}", path, addr);
 
-    let state = HttpServeState { path };
+    let state = HttpServeState { path: path.clone() };
 
     // axum router
     let router = Router::new()
         .route("/*path", get(file_handler))
+        .nest_service("/tower", ServeDir::new(path))
         .with_state(Arc::new(state));
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
@@ -42,6 +44,10 @@ async fn file_handler(
             format!("File {} not Found", p.display()),
         );
     } else {
+        // TODO: test p is a directory
+        // if it is a directory, list all files/subdirectories as
+        // <li><a href="/path/to/file">file name</a></li>
+        // <html><body><ul>...</ul></body></html>
         match tokio::fs::read_to_string(p).await {
             Ok(content) => {
                 info!("Read {} bytes", content.len());
@@ -52,5 +58,20 @@ async fn file_handler(
                 (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.trim().starts_with("[package]"));
     }
 }
